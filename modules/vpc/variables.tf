9d39eeaa8cff67733c7faf5d3e08fed3f3ce664c
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-25 17:00:46 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,2 +1,13 @@
-variable "cidr_block" { default = "10.0.0.0/16" }
-variable "public_subnets" { type = list(string) }
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_azs" {
+  description = "Availability zones for public subnets"
+  type        = list(string)
+}
