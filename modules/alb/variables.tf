b960f3db4427c968a2f9efeb278b0e91628fbd17
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-25 16:57:15 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,4 +1,9 @@
-variable "subnet_ids" { type = list(string) }
+variable "subnet_ids" { 
+    type = list(string) 
+}
 variable "security_group_id" {}
-variable "tg_name" { default = "api-tg" }
+variable "tg_name" { 
+    default = "api-tg" 
+}
 variable "vpc_id" {}
+
