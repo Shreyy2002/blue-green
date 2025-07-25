159d04393caf3d2f1f10654512b4339694fcf2b7
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-25 13:19:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,22 +13,24 @@ module "blue_ec2" {
   source            = "./modules/ec2"
   ami_id            = var.ami_id
   instance_type     = "t2.micro"
-  instance_count    = 1
+  instance_count    = var.instance_count
   subnet_id         = module.vpc.public_subnet_ids[0]
   security_group_id = module.sg.security_group_id
   key_name          = var.key_name
   name_prefix       = "blue"
+  user_data_path    = "${path.root}/scripts/user-data.sh"
 }
 
 module "green_ec2" {
   source            = "./modules/ec2"
   ami_id            = var.ami_id
   instance_type     = "t2.micro"
-  instance_count    = 1
+  instance_count    = var.instance_count
   subnet_id         = module.vpc.public_subnet_ids[1]
   security_group_id = module.sg.security_group_id
   key_name          = var.key_name
   name_prefix       = "green"
+  user_data_path    = "${path.root}/scripts/user-data.sh"
 }
 
 module "alb" {
