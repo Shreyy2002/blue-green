63512d1eaab57bce4e50a5839c9aa9ecc77206cf
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-25 16:59:10 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -3,6 +3,22 @@ resource "aws_security_group" "api_sg" {
   vpc_id      = var.vpc_id
   description = "Allow HTTP in, all out"
 
+ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow App Port 3000"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   ingress {
     from_port   = 80
     to_port     = 80
