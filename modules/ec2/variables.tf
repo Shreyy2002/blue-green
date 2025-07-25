bc3bd8806d1f6df40672240b2780b15f33fb683c
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-25 13:18:48 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,7 +1,36 @@
-variable "ami_id" {}
-variable "instance_type" { default = "t2.micro" }
-variable "instance_count" { default = 1 }
-variable "subnet_id" {}
-variable "security_group_id" {}
-variable "key_name" {}
-variable "name_prefix" { default = "blue" }
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "name_prefix" {
+  type    = string
+  default = "blue"
+}
+
+variable "user_data_path" {
+  type    = string
+  default = ""
+  description = "Absolute path to user-data.sh; if empty, no user data is used"
+}
