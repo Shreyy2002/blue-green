5415eb8944e48b7e7157027d31f48886ca50cfa2
Shreyy2002 <124439708+Shreyy2002@users.noreply.github.com>
2025-07-25 17:02:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "alb_dns" {
   value = module.alb.alb_dns
 }
+output "debug_az_to_subnets" {
+  value = local.az_to_subnets
+}
+output "debug_unique_subnet_ids" {
+  value = local.unique_subnet_ids
+}
+output "debug_public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+output "debug_public_subnet_azs" {
+  value = module.vpc.public_subnet_azs
+}
